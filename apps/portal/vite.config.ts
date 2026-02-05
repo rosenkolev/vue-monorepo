771db39780d5532ae9c837e509e6b9ff78a3b1e8
Rosen Kolev <rosen.kolev@hotmail.com>
2026-02-05 14:25:45 +0200

    Try to fix base url

--- apps/portal/vite.config.ts
+++ apps/portal/vite.config.ts
@@ -1,9 +1,10 @@
+import type { AliasOptions } from 'vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { getAliasFromTsConfig } from '../../vite-alias'
 
-const alias = getAliasFromTsConfig('../../tsconfig.base.json')
+const alias = getAliasFromTsConfig('../../tsconfig.base.json') as AliasOptions
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: process.env.APP_BASE_URL || '/',
